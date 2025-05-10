274622bfe48e0d4a233256de25be40e7b2e170a3
renzovaz <eusourenzo@gmail.com>
2025-05-10 15:10:09 -0300

    resoluçao do dever de logica de programação

---
+++ 1-ap-lp-sequencia-if/question01.py
@@ -0,0 +1,3 @@
+r=float(input('digite o valor da esfera'))
+volume=(4/3)*3.14*(r**4)
+print (volume)
--- AP1/question1.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-def mostra_valor(valor):
-    print(f'valor recebido: {valor}')
-
-if __name__ == "__main__":
-    mostra_valor(sys.argv[1])
