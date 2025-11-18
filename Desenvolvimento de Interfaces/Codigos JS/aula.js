3ce0799f1cee7f06cf1bd0d10d5a8fb0a8feea4f
Renzo Machado <eusourenzo@gmail.com>
2025-11-18 15:38:11 -0300

    edit

---
+++ Desenvolvimento de Interfaces/Codigos JS/aula.js
@@ -0,0 +1,17 @@
+console.log("Aprendendo JS")
+// const pi = 3.14;
+// console.log(pi);
+// pi = 3.1415;
+// console.log(pi)
+
+var x = 2025;
+console.log("O número é:"+ x)
+console.log("O número é:"+ x)
+x = 2026;
+console.log('o número é ${x}.');
+let it="be";
+console.log(it)
+it= "was";
+console.log(it);
+
+
